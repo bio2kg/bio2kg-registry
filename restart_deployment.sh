f875882933b123166b49fe9ef7b4a5dbd5ea1794
Vincent Emonet <vincent.emonet@gmail.com>
2021-07-09 15:52:15 +0200

    fix deployment

--- restart_deployment.sh
+++ restart_deployment.sh
@@ -4,4 +4,4 @@
 #git commit -m "Improvements"
 #git push
 
-ssh ids2 'cd /data/deploy-ids-tests/bio2kg-registry ; git pull ; docker-compose down ; docker-compose build --no-cache ; docker-compose up -d'
+ssh ids2 'cd /data/deploy-ids-tests/bio2kg-registry ; git pull ; docker-compose down ; docker-compose build --no-cache ; docker-compose up -d --remove-orphans'
