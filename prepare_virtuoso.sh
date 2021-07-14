87057d4d1d42e0b8f980b419efdb62eba5533336
Vincent Emonet <vincent.emonet@gmail.com>
2021-07-14 12:57:28 +0200

    fix dav password

--- prepare_virtuoso.sh
+++ prepare_virtuoso.sh
@@ -1,8 +1,4 @@
 
-# docker-compose exec virtuoso isql-v -U dba -P dba exec="ld_dir('/data/dumps', '*.nq', 'http://bio2rdf.org'); rdf_loader_run();"
-
-# docker exec -it bio2rdf5-virtuoso isql-v -U dba -P dba exec="ld_dir('/data/dumps', '*.nq', 'http://bio2rdf.org'); rdf_loader_run();"
-
 export DBA_PASSWORD=$(cat .env | sed 's/ELASTIC_PASSWORD=//g')
 
 wget http://download3.openlinksw.com/uda/vad-vos-packages/7.2/ods_framework_dav.vad
@@ -19,14 +15,13 @@ docker-compose exec virtuoso isql -U dba -P $DBA_PASSWORD exec="vad_install ('od
 
 # USER_CREATE ('dav', 'password', vector ('SQL_ENABLE', 'DAV_ENABLE', 'HOME', '/DAV/home/ldp') );
 docker-compose exec virtuoso isql -U dba -P $DBA_PASSWORD exec="DB.DBA.DAV_ADD_GROUP ('dav', 'dav', 'dav');"
-docker-compose exec virtuoso isql -U dba -P $DBA_PASSWORD exec="DB.DBA.DAV_ADD_USER ('dav', 'secret', 'dav', '110100000', 0, NULL, 'Test User Account', 'nobody@foo.bar', 'dav', 'dav');"
+docker-compose exec virtuoso isql -U dba -P $DBA_PASSWORD exec="DB.DBA.DAV_ADD_USER ('dav', '$DBA_PASSWORD', 'dav', '110100000', 0, NULL, 'Test User Account', 'nobody@foo.bar', 'dav', 'dav');"
 
-docker-compose exec virtuoso isql -U dav -P $DAV_PASSWORD exec="select DB.DBA.DAV_COL_CREATE ('/DAV/home/ldp/registry','110100000R', 'dav','dav','dav','dav');"
+docker-compose exec virtuoso isql -U dav -P $DBA_PASSWORD exec="select DB.DBA.DAV_COL_CREATE ('/DAV/home/ldp/registry','110100000R', 'dav','dav','dav','dav');"
 
 # http://docs.openlinksw.com/virtuoso/dbadm/
 # http://docs.openlinksw.com/virtuoso/fn_user_create/
 # http://docs.openlinksw.com/virtuoso/fn_dav_api_user/
 # http://docs.openlinksw.com/virtuoso/fn_dav_api_change/
 # http://docs.openlinksw.com/virtuoso/fn_dav_api_add/
-
 # Create folders: http://docs.openlinksw.com/virtuoso/fn_dav_api_add/
